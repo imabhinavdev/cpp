7a349f0e3bda92830241d2cd44e8fd7173a621fb
WiseVenom21 <abhinavas430@gmail.com>
2021-12-20 20:13:32 +0530

    First Try

---
+++ abhinav.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+#include <conio.h>
+
+void main()
+{
+    int a,b;
+    printf("Dear user please enter any number\n");
+    scanf("%d", &a);
+    // c = a * b;
+
+    // printf("Hello World %d",a+b);
+    // printf("Hello World %d\n",a-b);
+    // printf("Hello World %d\n",a/b);
+    // printf("Hello World %d\n",a*b);
+
+    if (a%2==0){
+        printf("Number is even");
+    
+    }
+    else{
+        printf("The number is odd");
+    }
+
+    // else if (a==18)
+    // {
+    //     printf("Show me your license");
+    // }
+
+    // else{
+    //     printf("You cannot drive");
+    // }
+
+
+
+
+
+    // return 0;
+}
