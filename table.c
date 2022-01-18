8b65e593eeec0b28fcc9fd66b8aad303e01221d9
Abhinav Singh <71094720+wisevenom21@users.noreply.github.com>
2022-01-18 21:49:35 +0530

    Create table.c
    Print table of a number that user enters.

---
+++ table.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+    int i, n;
+    printf("Enter any number:- ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= 10; i++)
+        printf("%d * %d = %d\n", n, i, n * i);
+}
