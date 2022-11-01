0c97b5411cff646ae0020541415b99f64f126b9c
Abhinav Singh <abhinavas430@gmail.com>
2022-11-02 01:17:28 +0530

    Update questionbank.cpp

--- Classwork/questionbank.cpp
+++ Classwork/questionbank.cpp
@@ -1,50 +1,32 @@
 #include <iostream>
-#include <stdlib.h>
 using namespace std;
 class Time
 {
-    int hour, minute, second;
-
 public:
+    int hh, ss, mm;
     void Input()
     {
-        cout << "Enter hour:- ";
-        cin >> hour;
-        cout << "Enter minute :- ";
-        cin >> minute;
-        cout << "Enter second:- ";
-        cin >> second;
-    }
-
-    void add(Time t1, Time t2, Time t3)
-    {
-
-        second = t1.second + t2.second + t3.second;
-        minute = t1.minute + t2.minute + t3.minute;
-        hour = t1.hour + t2.hour + t3.hour;
-        while (second >= 60)
-        {
-            second = second - 60;
-            minute++;
-        }
-        while (minute >= 60)
-        {
-            minute = minute - 60;
-            hour++;
-        }
-
-        system("CLS");
-        cout << "Hour :- " << hour;
-        cout << "\nMinute :- " << minute;
-        cout << "\nSecond:- " << second;
+        cout << "Enter the time(hh,mm,ss): ";
+        cin >> hh >> mm >> ss;
     }
 };
-
 int main()
 {
-    Time t1, t2, t3, t4;
-    t1.Input();
-    t2.Input();
-    t3.Input();
-    t4.add(t1, t2, t3);
+    Time T1, T2, T3;
+    T1.Input();
+    T2.Input();
+    T3.hh = T1.hh + T2.hh;
+    T3.mm = T1.mm + T2.mm;
+    T3.ss = T1.ss + T2.ss;
+    while (T3.ss >= 60)
+    {
+        T3.ss = T3.ss - 60;
+        T3.mm++;
+    }
+    while (T3.mm >= 60)
+    {
+        T3.mm = T3.mm - 60;
+        T3.hh++;
+    }
+    cout << "HH = " << T3.hh << " MM = " << T3.mm << " SS = " << T3.ss;
 }
